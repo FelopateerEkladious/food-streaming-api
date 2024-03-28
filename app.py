e4595e7e73fbe9b097c76fe9cb3edca4a0d1989d
projectprodezyre <113763751+projectprodezyre@users.noreply.github.com>
2024-03-28 15:59:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,3 +41,6 @@ async def fetch_data_api(year: int = Query(None), country: str = Query(None), ma
             return filtered_data
     except Exception as e:
         return {'error': str(e)}, 400
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
